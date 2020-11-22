895a211913d771efa0954471eabc033393379750
catherine91 <70351707+catherine91@users.noreply.github.com>
2020-11-22 11:53:39 -0700

    changes made by Catherine

---
+++ socialjs.js
@@ -0,0 +1,74 @@
+var athlete = {
+    name: "Dave",
+    age: 44,
+    height: "73 inches",
+    hometown: "Chicago, IL",
+    favoriteSport: [
+        {
+            sportName: "basketball",
+            teamInfo: [
+                {
+                practiceDay: "Saturday",
+                locationName: "East Valley Church",
+                public class Main {
+                    public static void main(String[] args) {
+                      String[] teamMembers = {"John", "Zack", "Thomas", "Josh"};
+                      System.out.println(teamMembers[0]);,
+                    ]
+                    }
+                  } 
+                },
+            ]
+        },
+    athleteFriends: [
+        {
+            name: "Carmen",
+            age: 31,
+            height: "60 inches",
+            hometown: "Cedar City, UT",
+            favoriteSport: [
+                {
+                    sportName: "softball",
+                    teamInfo: [
+                        {
+                        practiceDay: "Wednesday",
+                        locationName: "Elmwood Park",
+                        public class Main {
+                            public static void main(String[] args) {
+                              String[] teamMembers = {"Jessica", "Jane", "Emily", "Mary"};
+                              System.out.println(teamMembers[0]);,
+                            ]
+                            }
+                          } 
+                        }
+                    ]
+                },
+        {
+            name: "Mitchell",
+            age: 28,
+            height: "70 inches",
+            hometown: "San Diego, CA",
+            favoriteSport: [
+                {
+                    sportName: "football",
+                    teamInfo: [
+                        {
+                        practiceDay: "Monday", 
+                        locationName: "Liberty High School",
+                        public class Main {
+                            public static void main(String[] args) {
+                              String[] teamMembers = {"Rob", "Steven", "Aaron", "Terry"};
+                              System.out.println(teamMembers[0]);,
+                            ]
+                            }
+                          } 
+                        }
+                    ]
+                },
+            ]
+        },
+        {
+            
+        }
+    ]
+}
